2110998419f1d63a5c011fb1795179a67c5e9116
Milena <100636013+milena579@users.noreply.github.com>
2025-05-05 20:54:45 -0300

    Create Ponto3D.java

---
+++ T05/AlteraPonto3D/Ponto3D.java
@@ -0,0 +1,5 @@
+// 3. (Tarefa 5) Altere o código da classe Ponto 3D para que os métodos tenham
+// o modificador public, protected e private.
+// a. Para cada uma das alterações tente compilar e rodar.
+// b. Em algum dos casos ocorreu erro de compilação ou execução? Quais e
+// porque?
