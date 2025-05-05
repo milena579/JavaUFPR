e1e1c56d1ccbdca8ca17674cd9f593be9d018fdf
Milena <100636013+milena579@users.noreply.github.com>
2025-05-05 20:51:12 -0300

    Create SistemaAcademico.java

---
+++ T05/Universidade/SistemaAcademico.java
@@ -0,0 +1,28 @@
+// d. Crie uma outra classe (SistemaAcademico) que utilize objetos do tipo
+// Aluno. Nessa classe instancie um array de Alunos que armazene os
+// alunos criados. 
+//Crie um menu com as seguintes opções: 
+//1 – Cadastrar Aluno, 2 – Excluir aluno por nome, 3 – Listar Alunos, 4 – Matricular Aluno
+// em Disciplina, 5 – Cancelar Matrícula, 6 – Imprimir lista Alunos e
+// Disciplinas Matriculadas. Cada um destes itens do menu deve ter um
+// método associado com os seguintes métodos. Criar todos os métodos
+// estáticos:
+
+// 1. public static void cadastrarAluno(Aluno aluno)
+// 2. public static void excluirAlunoPorNome(String nomeAluno)
+// 3. public static Aluno[] listarAlunos()
+// 4. public static String matricularAlunoEmDisciplina(Aluno aluno,
+// String disciplina). Onde o retorno informa o que ocorreu com a
+// matricula do aluno, conforme especificado no método
+// fazMatricula.
+// 5. public static String cancelarMatricula(Aluno aluno, String
+// disciplina)
+// 6. public static String imprimirListaDeAlunoseDisciplinas ()
+
+// e. Quando o sistema iniciar deve pedir ao usuário para informar a
+// quantidade de alunos que serão cadastrados. Com essa informação
+// dimensione o array.
+
+// f. Quando o usuário optar pela opção 4, se for a primeira disciplina do
+// aluno, o sistema deve perguntar em quantas disciplinas o aluno deve ser
+// matriculado. Com essa informação dimensione o array de Strings.
